965717fa05f5adbe186cb53de908f4fb8ed3ce20
kggold4 <kggold4@gmail.com>
2023-12-11 22:19:42 +0200

    Fix sys path

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
-
+import sys
 from manager import app
 
 if __name__ == '__main__':
+    sys.path.insert(0, os.path.dirname(__file__))
     app.run(debug=bool(os.getenv("DEBUG_MODE", default=False)))
